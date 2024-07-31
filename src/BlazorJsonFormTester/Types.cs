b800d2bf02922505f575c2963ec393029b0ec068
Apollo3zehn <git@m1.apollo3zehn.net>
2024-07-31 21:59:24 +0200

    Add enum flags support

--- src/BlazorJsonFormTester/Types.cs
+++ src/BlazorJsonFormTester/Types.cs
@@ -1,6 +1,5 @@
 using System.ComponentModel.DataAnnotations;
 
-
 /// <summary>
 /// The rocket status
 /// </summary>
@@ -22,11 +21,55 @@ public enum RocketStatus
     Flight
 }
 
+[Flags]
+/// <summary>
+/// Mission targets
+/// </summary>
+public enum MissionTargets
+{
+    /// <summary>
+    /// Merkur
+    /// </summary>
+    Merkur = 1 << 0,
+
+    /// <summary>
+    /// Venus
+    /// </summary>
+    Venus = 1 << 1,
+
+    /// <summary>
+    /// Mars
+    /// </summary>
+    Mars = 1 << 2,
+
+    /// <summary>
+    /// Jupiter
+    /// </summary>
+    Jupiter = 1 << 3,
+
+    /// <summary>
+    /// Saturn
+    /// </summary>
+    Saturn = 1 << 4,
+
+    /// <summary>
+    /// Uranus
+    /// </summary>
+    Uranus = 1 << 5,
+
+    /// <summary>
+    /// Neptun
+    /// </summary>
+    Neptun = 1 << 6,
+}
+
 /// <summary>
 /// A rocket payload
 /// </summary>
+/// <param name="Name">Name</param>
 /// <param name="Weight">Weight in kg</param>
 public record Payload(
+    string Name,
     double Weight
 );
 
@@ -38,6 +81,7 @@ public record Payload(
 /// <param name="EnableTelemetry">Enable telemetry</param>
 /// <param name="Message">Message from mankind to the outer space</param>
 /// <param name="Status">Rocket status</param>
+/// <param name="MissionTargets">Mission targets</param>
 /// <param name="MainPayload">Main payload</param>
 /// <param name="AdditionalPayloads">Additional payloads</param>
 /// <param name="SpaceCoordinates">Space coordinates</param>
@@ -48,6 +92,7 @@ public record Rocket(
     bool EnableTelemetry,
     string Message,
     RocketStatus Status,
+    MissionTargets MissionTargets,
     Payload MainPayload,
     Payload[] AdditionalPayloads,
     int[] SpaceCoordinates
